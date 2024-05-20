135d7daf544419eaeab5d41fa213ba9135a13b1d
Ehsntb <ehsanei77@gmail.com>
2024-05-20 17:09:33 +0330

    111

--- ashkan ina template panel admin e/admin panel/NewAdmin/js/theme-customizer/customizer.js
+++
@@ -1,9 +0,0 @@
-if(localStorage.getItem("color"))
-    $("#color" ).attr("href", "../assets/css/"+localStorage.getItem("color")+".css" );
-if(localStorage.getItem("dark"))
-    $("body").attr("class", "rtl");
-(function() {
-})();
-
-$(".page-wrapper").attr("class", "page-wrapper compact-wrapper modern-sidebar");
-localStorage.setItem('page-wrapper', 'compact-wrapper modern-sidebar');
--- ashkan ina template panel admin e/admin panel/html/Users/userAddresses/user-address-detail.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('admin.layout.index')
-
-@section('profilecontent')
-    
-@endsection
