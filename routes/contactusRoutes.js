edf62271e08006608557e939b90aec853796b99f
Ehsntb <ehsanei77@gmail.com>
2024-05-20 17:45:51 +0330

    Update contactusRoutes.js

--- routes/contactusRoutes.js
+++ routes/contactusRoutes.js
@@ -1,16 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const contactusController = require('../controllers/contactusController');
+const contactusController = require("../controllers/contactusController");
 
-// Users Endpoint
-// module.exports = router.get(
-//   '/category/',
-//   categoryController.category,
-//   async (req, res) => {
-//     res.json();
-//   }
-// );
-
-module.exports = router.get('/contact_us', contactusController.contactUsHeader); // GET request
-module.exports = router.post('/contact_us', contactusController.submitContact); // POST request
+module.exports = router.get("/contact_us", contactusController.contactUsHeader); // GET request
+module.exports = router.post("/contact_us", contactusController.submitContact); // POST request
